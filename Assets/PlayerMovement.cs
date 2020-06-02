f1be766eda48e8b94fd56944d4e46633ba7c94be
Nathan Relyea <relyean19@up.edu>
2020-06-02 15:55:02 -0700

    Initial Big Boy Commit
    yeet them files

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    private float horizontalMove = 0f;
+    private bool jump = false;
+    private bool crouch = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    // called once every fixed amount of time (better for physics and movement)
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}

079b61f556c2cb604a25cab02075c61c8496e1db
Nathan Relyea <relyean19@up.edu>
2020-06-03 17:37:11 -0700

    Gun Created, Follows / Aims at Mouse

---
+++ Assets/Scripts/PlayerFaceCorrectDirection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFaceCorrectDirection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        float currentRelativeX = mousePosition.x - transform.position.x;
+
+        if (mousePosition.x - transform.position.x > 0)
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+
+        //Debug.Log("relative x: " + currentRelativeX);
+
+    }
+}

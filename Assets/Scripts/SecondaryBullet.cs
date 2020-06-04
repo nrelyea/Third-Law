9f41dd0bb7cfb2a0da44e69d7da6d78a984d4387
Nathan Relyea <relyean19@up.edu>
2020-06-03 21:02:30 -0700

    Gun shooting
    Two types of bullets, bullets detect what they are hitting and then are destroyed

---
+++ Assets/Scripts/SecondaryBullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondaryBullet : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Pulls bullet speed value from Gun object and applies it to the bullet
+        GameObject go = GameObject.Find("Gun");
+        WeaponFiring cs = go.GetComponent<WeaponFiring>();
+        rb.velocity = transform.right * cs.SecondaryBulletSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if (hitInfo.name != "Player" && hitInfo.name != "PrimaryBullet(Clone)" && hitInfo.name != "SecondaryBullet(Clone)")
+        {
+            Debug.Log("Secondary Hit " + hitInfo.name);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/WeaponFiring.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponFiring : MonoBehaviour
+{
+    public Transform firePoint;
+
+
+    public GameObject primaryBulletPrefab;
+    public float PrimaryBulletSpeed;
+
+    public GameObject secondaryBulletPrefab;
+    public float SecondaryBulletSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ShootPrimary();
+        }
+        else if (Input.GetButtonDown("Fire2"))
+        {
+            ShootSecondary();
+        }
+    }
+
+    void ShootPrimary()
+    {
+        Instantiate(primaryBulletPrefab, firePoint.position, firePoint.rotation);
+    }
+
+    void ShootSecondary()
+    {
+        Instantiate(secondaryBulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
